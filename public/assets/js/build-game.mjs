bc60fade51bbf72b48379242ccc5eb95b007119f
FesalBadday <fesal.n04@hotmail.com>
2021-12-08 16:09:27 -0700

    updated cards array

--- public/assets/js/build-game.mjs
+++ public/assets/js/build-game.mjs
@@ -1,15 +1,12 @@
 "use strict";
 
-import { startGame } from './start-game.mjs';
-import { errorMsg } from './client.js';
-export let cardsArray;
+export let cardsArray = [];
 
 // fetch data and return it into json
 export const runGame = async () => {
   try {
     let response = await fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6')
     let data = null
-    cardsArray = [];
 
     // check if response is ok
     if (!response.ok) {
@@ -20,30 +17,26 @@ export const runGame = async () => {
     }
 
     // load the cards
-    response = await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=52`)
+    response = await fetch(`https://deckofcardsapi.com/api/deck/${data.deck_id}/draw/?count=312`)
 
     // check if response is ok
     if (!response.ok) {
       // if not throw an error
       throw new Error('Not 200 OK');
     } else {
-      data = await response.json() // return json file with array of 52 cards
+      data = await response.json() // return json file with array of 312 cards
 
       for (let i = 0; i < data.cards.length; i++) {
         cardsArray.push(data.cards[i]);
       }
 
-      startGame();
+      //startGame();
     }
 
   } catch (e) { // catch errors
     console.log('Caught an error!', e)
-    document.querySelector(".game-section").classList.toggle("hide-toggle");
-    // print error message
-    errorMsg.innerHTML =
-      `<div class="error">
-        <p>An error occurred while loading the game. Please try again later.</p>
-        <img src="assets/images/pixeltrue-error.svg" alt="pixeltrue-error">
-      </div>`;
+    location.replace("404");
   }
 }
+
+runGame();
